cb7ab0ec040c27cac7bc15f8e0cf727f6ed1bf3a
FrancoRutigliano <francorutigliano11@gmail.com>
2024-01-03 14:32:18 -0300

    Casos funcionando

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -16,6 +16,12 @@ func main() {
 
 	add := flag.Bool("add", false, "add a new todo")
 
+	complete := flag.Int("complete", 0, "mark a todo as completed")
+
+	del := flag.Int("del", 0, "delete a todo")
+
+	list := flag.Bool("list", false, "list all todos")
+
 	flag.Parse()
 
 	todos := &todo.Todos{}
@@ -27,13 +33,43 @@ func main() {
 
 	switch {
 	case *add:
-		todos.Add("sample todo")
+		var task string
+		fmt.Print("Task to add: ")
+		fmt.Scanln(&task)
+		todos.Add(task)
 		err := todos.Store(todoFile)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "invalid command")
+			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
 		}
 
+	case *complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+	case *del > 0:
+		err := todos.Delete(*del)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+	case *list:
+		todos.Print()
 	default:
 		fmt.Fprintln(os.Stdout, "invalid command")
 		os.Exit(1)
