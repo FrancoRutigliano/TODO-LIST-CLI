214ac4016f6a7af5db80ad9d8bf485c4e0a2dfc5
FrancoRutigliano <francorutigliano11@gmail.com>
2024-01-03 18:04:37 -0300

    Detalles en la terminal(Colores, chars unicode)

--- todo.go
+++ todo.go
@@ -104,10 +104,18 @@ func (t *Todos) Print() {
 
 	for idx, item := range *t {
 		idx++
+
+		task := blue(item.Task)
+		done := blue("no")
+		if item.Done {
+			task = green(fmt.Sprintf("\u2705 %s", item.Task))
+			done = green("yes")
+		}
+
 		cells = append(cells, *&[]*simpletable.Cell{
 			{Text: fmt.Sprintf("%d", idx)},
-			{Text: item.Task},
-			{Text: fmt.Sprintf("%t", item.Done)},
+			{Text: task},
+			{Text: done},
 			{Text: item.CreatedAt.Format(time.RFC822)},
 			{Text: item.CompletedAt.Format(time.RFC822)},
 		})
@@ -116,10 +124,22 @@ func (t *Todos) Print() {
 	table.Body = &simpletable.Body{Cells: cells}
 
 	table.Footer = &simpletable.Footer{Cells: []*simpletable.Cell{
-		{Align: simpletable.AlignCenter, Span: 5, Text: "Your todos are here"},
+		{Align: simpletable.AlignCenter, Span: 5, Text: red(fmt.Sprintf("You have Pending %d todos", t.CountPending()))},
 	}}
 
 	table.SetStyle(simpletable.StyleUnicode)
 
-	table.Print()
+	table.Println()
+}
+
+func (t *Todos) CountPending() int {
+	total := 0
+
+	for _, item := range *t {
+		if !item.Done {
+			total++
+		}
+	}
+
+	return total
 }
