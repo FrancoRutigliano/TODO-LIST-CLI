e2053f43735c98250500446e05338ee37aff00f6
FrancoRutigliano <francorutigliano11@gmail.com>
2024-01-03 09:47:58 -0300

    Flag 'add' funcionando crea el todos.json

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	todo "github.com/FrancoRutigliano"
+)
+
+const (
+	todoFile = ".todos.json"
+)
 
 func main() {
-	fmt.Println("Hello Wordl")
+
+	add := flag.Bool("add", false, "add a new todo")
+
+	flag.Parse()
+
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		todos.Add("sample todo")
+		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "invalid command")
+			os.Exit(1)
+		}
+
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(1)
+	}
+
 }
